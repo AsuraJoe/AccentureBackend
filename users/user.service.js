f1f5afddd122c9f15e89ecca6954719d22142a93
AsuraJoe <41365507+AsuraJoe@users.noreply.github.com>
2018-10-30 17:18:35 -0400

    Initial commit

---
+++ users/user.service.js
@@ -0,0 +1,117 @@
+﻿const config = require('config.json');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const db = require('_helpers/db');
+const User = db.User;
+const nodemailer = require('nodemailer');
+var async = require('async');
+const crypto = require('crypto');
+const smtpTransport = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: '5star.cosc412',
+        pass: '5*rbhdldmftn'
+    }
+});
+module.exports = {
+    authenticate,
+    getAll,
+    getById,
+    create,
+    update,
+    delete: _delete,
+    sendRequest
+};
+
+async function authenticate({ username, password }) {
+    const user = await User.findOne({ username });
+    if (user && bcrypt.compareSync(password, user.hash)) {
+        const { hash, ...userWithoutHash } = user.toObject();
+        const token = jwt.sign({ sub: user.id }, config.secret);
+        return {
+            ...userWithoutHash,
+            token
+        };
+    }
+}
+
+async function getAll() {
+    return await User.find().select('-hash');
+}
+
+async function getById(id) {
+    return await User.findById(id).select('-hash');
+}
+
+async function sendRequest({email}){
+    const user = await User.findOne({ email });
+    if(user) {
+        var token;
+        crypto.randomBytes(20, function(err, buffer) {
+            token = buffer.toString('hex');
+        });
+        User.findByIdAndUpdate(user.id, { reset_password_token: token, reset_password_expires: Date.now() + 86400000 }, { upsert: true, new: true },
+        function(err, result){
+            if(err){
+                console.log(err);
+            }
+            console.log("RESULT: " + result);
+        });
+        var data = {
+            to: user.mail,
+            from: '5star.cosc412@gmail.com',
+            subject: 'Password reset!',
+            html:'<p>testing....</p>'
+          };
+    
+          smtpTransport.sendMail(data, function(err) {
+            if (!err) {
+              return res.json({ message: 'Kindly check your email for further instructions' });
+            } else {
+              return res.json({message: {err,data}});
+            }
+          });
+    }
+    else throw 'User Not Found';
+}
+
+async function create(userParam) {
+    // validate
+    if (await User.findOne({ username: userParam.username })) {
+        throw 'Username "' + userParam.username + '" is already taken';
+    }
+
+    const user = new User(userParam);
+
+    // hash password
+    if (userParam.password) {
+        user.hash = bcrypt.hashSync(userParam.password, 10);
+    }
+
+    // save user
+    await user.save();
+}
+
+async function update(id, userParam) {
+    const user = await User.findById(id);
+
+    // validate
+    if (!user) throw 'User not found';
+    if (user.username !== userParam.username && await User.findOne({ username: userParam.username })) {
+        throw 'Username "' + userParam.username + '" is already taken';
+    }
+
+    // hash password if it was entered
+    if (userParam.password) {
+        userParam.hash = bcrypt.hashSync(userParam.password, 10);
+    }
+
+    // copy userParam properties to user
+    Object.assign(user, userParam);
+
+    await user.save();
+}
+
+async function _delete(id) {
+    await User.findByIdAndRemove(id);
+}
---
+++ users/users.controller.js
@@ -0,0 +1,113 @@
+﻿const express = require('express');
+const router = express.Router();
+const userService = require('./user.service');
+const nodemailer = require('nodemailer');
+var async = require('async');
+const crypto = require('crypto');
+const db = require('_helpers/db');
+var User = db.User;
+const smtpTransport = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: '5star.cosc412',
+        pass: '5*rbhdldmftn'
+    }
+})
+
+// routes
+router.post('/authenticate', authenticate);
+router.post('/register', register);
+router.get('/', getAll);
+router.get('/current', getCurrent);
+router.get('/:id', getById);
+router.put('/:id', update);
+router.delete('/:id', _delete);
+router.post('/resetRequest', forgotPass);
+
+module.exports = router;
+
+function authenticate(req, res, next) {
+    userService.authenticate(req.body)
+        .then(user => user ? res.json(user) : res.status(400).json({ message: 'Username or password is incorrect' }))
+        .catch(err => next(err));
+}
+
+function register(req, res, next) {
+    userService.create(req.body)
+        .then(() => res.json({}))
+        .catch(err => next(err));
+}
+
+function getAll(req, res, next) {
+    userService.getAll()
+        .then(users => res.json(users))
+        .catch(err => next(err));
+}
+
+function getCurrent(req, res, next) {
+    userService.getById(req.user.sub)
+        .then(user => user ? res.json(user) : res.sendStatus(404))
+        .catch(err => next(err));
+}
+
+function getById(req, res, next) {
+    userService.getById(req.params.id)
+        .then(user => user ? res.json(user) : res.sendStatus(404))
+        .catch(err => next(err));
+}
+
+function update(req, res, next) {
+    userService.update(req.params.id, req.body)
+        .then(() => res.json({}))
+        .catch(err => next(err));
+}
+
+function _delete(req, res, next) {
+    userService.delete(req.params.id)
+        .then(() => res.json({}))
+        .catch(err => next(err));
+}
+
+function forgotPass(req, res){
+  async.waterfall([
+    function(done) {
+      crypto.randomBytes(20, function(err, buf) {
+        var token = buf.toString('hex');
+        done(err, token);
+      });
+    },
+    function(token, done) {
+      User.findOne({ email: req.body.email }, function(err, user) {
+        if (!user) {
+          req.flash('error', 'No account with that email address exists.');
+          return res.redirect('/resetRequest');
+        }
+
+        user.reset_password_Token = token;
+        user.reset_password_Expires = Date.now() + 3600000; // 1 hour
+
+        user.save(function(err) {
+          done(err, token, user);
+        });
+      });
+    },
+    function(token, user, done) {
+      var mailOptions = {
+        to: user.email,
+        from: '5star.cosc412@gmail.com',
+        subject: 'Password Reset',
+        text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
+          'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+          'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+          'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+      };
+      smtpTransport.sendMail(mailOptions, function(err) {
+        req.flash('info', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
+        done(err, 'done');
+      });
+    }
+  ], function(err) {
+    if (err) return next(err);
+    res.redirect('/resetRequest');
+  });
+}
